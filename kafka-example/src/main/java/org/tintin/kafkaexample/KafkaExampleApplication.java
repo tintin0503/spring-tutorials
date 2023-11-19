7e69999c769e6944e08c91699c4947273cba9b13
tintin0503 <quanptit2801@gmail.com>
2023-11-19 07:43:09 +0700

    add project with modules

---
+++ kafka-example/src/main/java/org/tintin/kafkaexample/KafkaExampleApplication.java
@@ -0,0 +1,10 @@
+package org.tintin.kafkaexample;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaExampleApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
